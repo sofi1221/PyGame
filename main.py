3778f2839f1f5fed04ce3d5469a955cf7d29c9c1
sofi1221 <sofi1221@mail.ru>
2020-01-16 16:55:34 +0500

    Скелет для приложения

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print('Hello, world!')
+import pygame
+
+pygame.init()
+size = width, height = 1100, 700
+screen = pygame.display.set_mode(size)
+
+def draw():
+    pass
+
+
+draw()
+pygame.display.flip()
+running = True
+clock = pygame.time.Clock()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    draw()
+
+    pygame.display.flip()
+    clock.tick(24)
